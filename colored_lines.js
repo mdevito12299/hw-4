1be6eee507cdcc976aba27a8319bf8b42614d9f7
mdevito12299 <43148221+mdevito12299@users.noreply.github.com>
2018-10-01 14:49:14 -0700

    Create colored_lines.js

---
+++ colored_lines.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB)
+}
+
+function draw() {
+  background(255);
+  
+
+  for (var x = 10; x < width-10; x = x + 10) {
+   stroke(x,300,300);
+    line(x, height/2, mouseX, mouseY);
+    
+  }
+}
